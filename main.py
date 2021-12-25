d1edb5c924c27601058931dbcdc22dbc6608c80f
Abhishek <50742393+ashriv2020@users.noreply.github.com>
2021-12-25 06:00:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title('Another py web')
+st.subheader('Welcome to my website!')
+st.write(''' Here you can learn how to build website using python.
+Great Journey need to start early!!
+Let's Start....
+''')
