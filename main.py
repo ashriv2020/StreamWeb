963457b93bfb990cb3a0d04b2bc977a13077bc33
Abhishek <50742393+ashriv2020@users.noreply.github.com>
2021-12-25 06:21:52 +0000

    added pandas dataframe

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import streamlit as st
+import pandas
+
+data= {
+  'Series_1':[1,3,5,7,9],
+  'Series_2':[10,30,50,70,90],
+}
+
 st.title('Another py web')
 st.subheader('Welcome to my website!')
 st.write(''' Here you can learn how to build website using python.
 Great Journey need to start early!!
 Let's Start....
 ''')
+df = pandas.DataFrame(data)
+st.write(df);
